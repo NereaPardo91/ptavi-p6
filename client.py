65f276ddf70f6cd3e5e81347b8d17a33d6bcd814
NereaPardo91 <n.pardonavarro1991@gmail.com>
2016-11-14 17:30:17 +0100

    Recibo respuesta INVITE

--- client.py
+++ client.py
@@ -10,25 +10,27 @@ import sys
 try:
 	METODO = sys.argv[1]
 	DATOS = sys.argv[2]
-	PORT = int(DATOS.split(":")[-1])
 except:
 	sys.exit("Usage: python client.py receptor@IPreceptor:puertoSIP")
 
-#AQUI VAN TODOS LOS METODOS INVITE, ACK, BYE
+PORT = int(DATOS.split(":")[-1])
+DIR_SIP = DATOS.split(":")[0]
+
 if METODO == 'INVITE':
-	LINE = METODO
+	LINE = METODO + ' ' + 'sip:' + DIR_SIP + ' ' + 'SIP/2.0'
+if METODO == 'BYE':
+	LINE = DIR_SIP + ' ' + 'SIP/2.0'
 
 # Creamos el socket, lo configuramos y lo atamos a un servidor/puerto
 my_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 my_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 my_socket.connect(('127.0.0.1', PORT))
 
-
 print("Enviando: " + LINE)
 my_socket.send(bytes(LINE, 'utf-8') + b'\r\n')
 data = my_socket.recv(1024)
 
-print('Recibido -- ', data.decode('utf-8'))
+print('Recibido... ', data.decode('utf-8'))
 print("Terminando socket...")
 
 # Cerramos todo
