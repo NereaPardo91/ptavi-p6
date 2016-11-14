ac47963197be4cb7a580a9473c9e9fa627904384
NereaPardo91 <n.pardonavarro1991@gmail.com>
2016-11-14 17:52:40 +0100

    Envio de ACK

--- server.py
+++ server.py
@@ -20,18 +20,14 @@ class EchoHandler(socketserver.DatagramRequestHandler):
         line = line.decode('utf-8').split()
 
         if line[0] == 'INVITE':
-            self.wfile.write(b'SIP/2.0 100 Trying' + ' ' + b'SIP/2.0 180 Ring')
-        if line[0] == 'BYE':
+            self.wfile.write(b'SIP/2.0 100 Trying' + b' ' + b'SIP/2.0 180 Ring' + b' ' + b'SIP/2.0 200 OK')
+        elif line[0] == 'BYE':
             self.wfile.write(b'adios')
+        else:
+            self.wfile.write(b'SIP/2.0 405 Method Not Allowed')
 
 if __name__ == "__main__":
     # Creamos servidor de eco y escuchamos
     serv = socketserver.UDPServer(('', P), EchoHandler)
     print("Lanzando servidor UDP de eco...")
     serv.serve_forever()
-
-    #Debemos transformalo para llamarle de esta manera:
-            # python.py metodo (INVITE O BYE) receptor@IPreceptor:puertoSIP
-            # client.py BYE robin@193.147.73.20:5555
-            # sino que nos salga el usage error
-            #al servidor debemos darle un puerto distinto al 5060
