177036c37042493b91f28e8d795752ccffa4a30e
NereaPardo91 <n.pardonavarro1991@gmail.com>
2016-11-16 12:51:52 +0100

    error connected refused

--- client.py
+++ client.py
@@ -15,14 +15,9 @@ except:
 
 PORT = int(DATOS.split(":")[-1])
 DIR_SIP = DATOS.split(":")[0]
-#ACK = DATOS.split(":")[0] + 'SIP/2.0'
+LINE = METODO + ' ' + DIR_SIP + ' ' + 'SIP/2.0'
+ACK_MSG = 'ACK' + ' ' + DIR_SIP + ' ' + 'SIP/2.0'
 
-if METODO == 'INVITE':
-	LINE = METODO + ' ' + 'sip:' + DIR_SIP + ' ' + 'SIP/2.0'
-if METODO == 'BYE':
-	LINE = DIR_SIP + ' ' + 'SIP/2.0'
-
-# Creamos el socket, lo configuramos y lo atamos a un servidor/puerto
 my_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 my_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 my_socket.connect(('127.0.0.1', PORT))
@@ -30,16 +25,14 @@ my_socket.connect(('127.0.0.1', PORT))
 print("Enviando: " + LINE)
 my_socket.send(bytes(LINE, 'utf-8') + b'\r\n')
 data = my_socket.recv(1024)
+print('Respuesta Server... ', data.decode('utf-8'))
 
-ACK = data.decode('utf-8')
-#print(ACK)
-ACK_MSG = 'OK SIP/2.0'
-#print(ACK)
+Reply_Server = data.decode('utf-8')
 
-if ACK == ('SIP/2.0 100 Trying SIP/2.0 180 Ring SIP/2.0 200 OK'):
+if Reply_Server == ('SIP/2.0 100 Trying SIP/2.0 180 Ring SIP/2.0 200 OK'):
+	print('Enviando ACK... ')
 	my_socket.send(bytes(ACK_MSG, 'utf-8') + b'\r\n')
 	
-print('Recibido... ', data.decode('utf-8'))
 print("Terminando socket...")
 
 # Cerramos todo
